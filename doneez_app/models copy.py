5ed990e73d22de8d46c1349ec22b712626d1cd8d
Greg Ruiz <gregruiz@me.com>
2022-06-01 09:37:09 -0400

    Replaced default Django User model with CustomUser model from AbstractBaseUser and set username=CICharField on postgres.  Added signup exit button and hid dashboard menu when not authenticated.

--- doneez_app/models copy.py
+++ doneez_app/models copy.py
@@ -1,88 +1,19 @@
 from django import forms
 from django.db import models
-from django.utils import timezone
 from django.core.validators import URLValidator
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.conf import settings
 
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, UserManager
-from django.contrib.auth.validators import ASCIIUsernameValidator
-from django.contrib.postgres.fields import CICharField
-from django.core.mail import send_mail
-from django.utils.translation import gettext_lazy as _
 
 
+class CustomUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        case_insensitive_username_field = '{}__iexact'.format(self.model.USERNAME_FIELD)
+        return self.get(**{case_insensitive_username_field: username})
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    """
-    This model replaces the Django default User model
-    to allow current and future customizations.
-    It is nearly an exact copy of the default User model,
-    with a few customizations indicated in the comments throughout.
-        
-    """
 
-    # Customize the validator to use Ascii instead of Unicode
-    username_validator = ASCIIUsernameValidator()
-
-    # Customize the username field to be case-insensitive, using the Postgres CITextExtension implementation
-    username = CICharField(
-        _('username'),
-        max_length=150,
-        unique=True,
-        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        validators=[username_validator],
-        error_messages={
-            'unique': _("A user with that username already exists."),
-        },
-    )
-    first_name = models.CharField(_('first name'), max_length=150, blank=True)
-    last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    email = models.EmailField(_('email address'), blank=True)
-    is_staff = models.BooleanField(
-        _('staff status'),
-        default=False,
-        help_text=_('Designates whether the user can log into this admin site.'),
-    )
-    is_active = models.BooleanField(
-        _('active'),
-        default=True,
-        help_text=_(
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
-        ),
-    )
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-
-    objects = UserManager()
-
-    EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
-
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-
-    def clean(self):
-        super().clean()
-        self.email = self.__class__.objects.normalize_email(self.email)
-
-    def get_full_name(self):
-        """
-        Return the first_name plus the last_name, with a space in between.
-        """
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
-
-    def get_short_name(self):
-        """Return the short name for the user."""
-        return self.first_name
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        """Send an email to this user."""
-        send_mail(subject, message, from_email, [self.email], **kwargs)
-
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
 
 
 class BusinessType(models.Model):
